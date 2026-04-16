95d4a4dd2f343ed11d618cac83cbe4b2c0879d78
ManuIshan <manueshanchekkali@gmail.com>
2026-04-16 22:43:00 +0530

    fix serializer properly

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,54 +1,45 @@
 from rest_framework import serializers
 from .models import Project, ProjectImage
 
-
 class ProjectImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProjectImage
-        fields = ["id", "image"]
+ image = serializers.SerializerMethodField()
 
-
-class ProjectSerializer(serializers.ModelSerializer):
-    gallery = ProjectImageSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Project
-        fields = "__all__" 
-        
-from rest_framework import serializers
-from .models import Project, ProjectImage
-
-class ProjectImageSerializer(serializers.ModelSerializer):
-   image = serializers.SerializerMethodField()
 
 class Meta:
     model = ProjectImage
     fields = ["id", "image"]
 
 def get_image(self, obj):
-    request = self.context.get('request')
+    request = self.context.get("request", None)
     if obj.image:
-        return request.build_absolute_uri(obj.image.url)
+        if request:
+            return request.build_absolute_uri(obj.image.url)
+        return obj.image.url
     return None
 
-class ProjectSerializer(serializers.ModelSerializer):
 
-    gallery = ProjectImageSerializer(many=True, read_only=True)
-    cover_image = serializers.SerializerMethodField()
-    main_image = serializers.SerializerMethodField()
+class ProjectSerializer(serializers.ModelSerializer):
+ gallery = ProjectImageSerializer(many=True, read_only=True)
+ cover_image = serializers.SerializerMethodField()
+ main_image = serializers.SerializerMethodField()
+
 
 class Meta:
     model = Project
     fields = "__all__"
 
 def get_cover_image(self, obj):
-    request = self.context.get('request')
+    request = self.context.get("request", None)
     if obj.cover_image:
-        return request.build_absolute_uri(obj.cover_image.url)
+        if request:
+            return request.build_absolute_uri(obj.cover_image.url)
+        return obj.cover_image.url
     return None
 
 def get_main_image(self, obj):
-    request = self.context.get('request')
+    request = self.context.get("request", None)
     if obj.main_image:
-        return request.build_absolute_uri(obj.main_image.url)
+        if request:
+            return request.build_absolute_uri(obj.main_image.url)
+        return obj.main_image.url
     return None
