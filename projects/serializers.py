1cbeb94937e922200880739b987348b3c3291f70
ManuIshan <manueshanchekkali@gmail.com>
2026-04-16 22:06:44 +0530

    final url fix

--- projects/serializers.py
+++ projects/serializers.py
@@ -13,4 +13,42 @@ class ProjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Project
-        fields = "__all__"
+        fields = "__all__" 
+        
+from rest_framework import serializers
+from .models import Project, ProjectImage
+
+class ProjectImageSerializer(serializers.ModelSerializer):
+   image = serializers.SerializerMethodField()
+
+class Meta:
+    model = ProjectImage
+    fields = ["id", "image"]
+
+def get_image(self, obj):
+    request = self.context.get('request')
+    if obj.image:
+        return request.build_absolute_uri(obj.image.url)
+    return None
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    gallery = ProjectImageSerializer(many=True, read_only=True)
+    cover_image = serializers.SerializerMethodField()
+    main_image = serializers.SerializerMethodField()
+
+class Meta:
+    model = Project
+    fields = "__all__"
+
+def get_cover_image(self, obj):
+    request = self.context.get('request')
+    if obj.cover_image:
+        return request.build_absolute_uri(obj.cover_image.url)
+    return None
+
+def get_main_image(self, obj):
+    request = self.context.get('request')
+    if obj.main_image:
+        return request.build_absolute_uri(obj.main_image.url)
+    return None
--- projects/views.py
+++ projects/views.py
@@ -1,25 +1,34 @@
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
 from .models import Project
 from .serializers import ProjectSerializer
-
 
 class ProjectListView(generics.ListAPIView):
     serializer_class = ProjectSerializer
 
-    def get_queryset(self):
-        queryset = Project.objects.all().order_by("-created_at")
-        show_in_home = self.request.query_params.get("show_in_home")
-        if show_in_home is not None:
-            show_in_home = show_in_home.lower()
-            if show_in_home in ["1", "true", "yes"]:
-                queryset = queryset.filter(show_in_home=True)
-            elif show_in_home in ["0", "false", "no"]:
-                queryset = queryset.filter(show_in_home=False)
-        return queryset
+
+def get_queryset(self):
+    queryset = Project.objects.all().order_by("-created_at")
+    show_in_home = self.request.query_params.get("show_in_home")
+
+    if show_in_home is not None:
+        show_in_home = show_in_home.lower()
+        if show_in_home in ["1", "true", "yes"]:
+            queryset = queryset.filter(show_in_home=True)
+        elif show_in_home in ["0", "false", "no"]:
+            queryset = queryset.filter(show_in_home=False)
+
+    return queryset
+
+def get_serializer_context(self):
+    return {"request": self.request}
 
 class ProjectDetailView(generics.RetrieveAPIView):
-    serializer_class = ProjectSerializer
+  serializer_class = ProjectSerializer
 
-    def get_object(self):
-        slug = self.kwargs.get("slug")
-        return get_object_or_404(Project, slug__iexact=slug)
+def get_object(self):
+    slug = self.kwargs.get("slug")
+    return get_object_or_404(Project, slug__iexact=slug)
+
+def get_serializer_context(self):
+    return {"request": self.request}
